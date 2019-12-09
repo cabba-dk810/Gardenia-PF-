310bf0d2c1c47e2048e8c3bf3ef57448ac0d6b96
Daichi0502 <fswamd.9@gmail.com>
2019-12-09 07:37:26 +0000

    [Add] いいね機能の追加 #9

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
   get 'users/:id/follower' => 'users#follower', as: 'follower'
   get 'users/:id/following' => 'users#following', as: 'following'
 
-  resources :post_gardens, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :post_gardens, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :likes, only: [:create, :destroy]
+  end
   root to: 'post_gardens#index'
   get 'post_gardens/about' => 'post_gardens#about', as: 'about'
   get 'post_gardens/:id/new_open_garden' => 'post_gardens#new_open_garden', as: 'new_open_garden'
