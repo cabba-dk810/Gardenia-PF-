882dc6b4171c2daa073ca1434219a4e381048381
Daichi0502 <fswamd.9@gmail.com>
2019-12-22 08:11:22 +0000

    [Fix]トップページのレイアウト #35

--- app/controllers/open_days_controller.rb
+++ app/controllers/open_days_controller.rb
@@ -3,8 +3,6 @@ class OpenDaysController < ApplicationController
 	def create
 		@post_garden = PostGarden.find(params[:open_day][:post_garden_id])
 		@open_day = OpenDay.new(open_day_params)
-		@open_day.before_start_time = @open_day.start_time
-		@open_day.before_end_time = @open_day.end_time
 		# 直接値を代入する方法
 		# @open_day.start_time = DateTime.parse(params[:open_day]["start_time(1i)"] + "-" + params[:open_day]["start_time(2i)"] + "-" + params[:open_day]["start_time(3i)"] + " " + params[:open_day]["start_time(4i)"] + ":" + params[:open_day]["start_time(5i)"]).to_s
 		# @open_day.end_time = DateTime.parse(params[:open_day]["end_time(1i)"] + "-" + params[:open_day]["end_time(2i)"] + "-" + params[:open_day]["end_time(3i)"] + " " + params[:open_day]["end_time(4i)"] + ":" + params[:open_day]["end_time(5i)"]).to_s
