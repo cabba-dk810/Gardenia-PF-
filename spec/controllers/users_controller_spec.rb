c2a2aca561bcb4397f7968feede892211cafb68b
Daichi0502 <fswamd.9@gmail.com>
2020-02-26 06:56:46 +0000

    [Add] テストコードの追加（Userモデル） #78

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,4 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+
+	describe 'マイページ' do
+
+      	context "マイページが正しく表示される" do
+	        before do
+	        	get :show
+        	end
+
+        	it 'リクエストは200 OKとなること' do
+          		expect(response.status).to eq 200
+        	end
+      	end
+
+    end
+
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,13 +2,13 @@
 
 FactoryBot.define do
   factory :user do
-    sequence(:user_name) { '加藤大地' }
-    sequence(:postal_code) { 3_800_803 }
-    sequence(:prefecture) { 12 }
-    sequence(:address) { '長野県長野市三輪3-9-12' }
-    sequence(:phone_number) { '07010400683' }
-    sequence(:profile_text) { 'よろしくお願いします' }
-    sequence(:email) { 'fswamd@yahoo.co.jp' }
-    sequence(:password) { '1111111' }
+    user_name { 'daichi' }
+    postal_code { 3800803 }
+    prefecture { 12 }
+    address { '長野県長野市三輪3-9-12' }
+    phone_number { '07010400683' }
+    profile_text { 'よろしくお願いします' }
+    email { 'fswamd@yahoo.co.jp' }
+    password { '1111111' }
   end
 end
